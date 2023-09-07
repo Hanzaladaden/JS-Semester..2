72f3534058dac3644fdc12eb9c528db4cb59cedc
Hanzaladaden <141207746+Hanzaladaden@users.noreply.github.com>
2023-09-08 00:06:58 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+let hello1 = (name) => console.log(`hanzala ${name}`)
+hello1("aptech garden center")
+
+var sum1="3";
+var sum2=23;
+if(sum1 ==3 && sum2 =="23")
+{document.write("yes")}
+else
+{document.write("no")}
+
+var sum1=prompt("Insert The Sum Value 1");
+var sum2=prompt("Insert The Sum Value 2");
+if(sum1 ==512 && sum2 ==1024)
+{
+    alert("Success")
+}
+else
+{
+    alert("Failed")
+}
